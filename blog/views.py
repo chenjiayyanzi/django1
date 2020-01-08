f159ae42ecf8d226617fe0a5b3e565e9aecb1170
chenjiayyanzi <chenjiayuyanzi@163.com>
2020-01-08 11:06:58 +0100

    add variables in home page

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Jiayu chen',
+        'title': 'First blog',
+        'content': 'this is the first blog',
+        'post_date': 'August 28 2019'
+    },
+    {
+        'author': 'Clem Cherrey',
+        'title': 'Second blog',
+        'content': 'this is the second blog',
+        'post_date': 'August 29 2019'
+    }
+]
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts1': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html')
