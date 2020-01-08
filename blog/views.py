b822267054e8d5b1e46c100b4fdc5896e4bcc5aa
chenjiayyanzi <chenjiayuyanzi@163.com>
2020-01-08 17:34:27 +0100

    add blog in database and register blog in admin page

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Jiayu chen',
-        'title': 'First blog',
-        'content': 'this is the first blog',
-        'post_date': 'August 28 2019'
-    },
-    {
-        'author': 'Clem Cherrey',
-        'title': 'Second blog',
-        'content': 'this is the second blog',
-        'post_date': 'August 29 2019'
-    }
-]
+from .models import Post
 
 
 # Create your views here.
 def home(request):
     context = {
-        'posts1': posts
+        'posts1': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html', {'title':'Django-Blog-about'})
+    return render(request, 'blog/about.html', {'title': 'Django-Blog-about'})
