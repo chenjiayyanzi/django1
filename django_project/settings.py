3d1edd99951af6f3ccb207f18279f587737c4fdb
chenjiayyanzi <chenjiayuyanzi@163.com>
2020-01-10 17:56:48 +0100

    add sign up form

--- django_project/settings.py
+++ django_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'register.apps.RegisterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
