286381630c7a36e4c022f7bc4d24f4caf27d69df
chenjiayyanzi <chenjiayuyanzi@163.com>
2020-01-17 15:03:51 +0100

    pagination

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -18,7 +19,18 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts1'
     ordering = ['-post_date']
+    paginate_by = 2
 
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts1'
+    ordering = ['-post_date']
+    paginate_by = 2
+
+    def get_queryset(self):
+        user = get_object_or_404(User,username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-post_date')
 
 class PostDetailView(DetailView):
     model = Post
@@ -51,11 +63,13 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     success_url = '/'
+
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author:
             return True
         return False
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'Django-Blog-about'})
