3df89678108707242e13afd71dd5f494c959407b
chenjiayyanzi <chenjiayuyanzi@163.com>
2020-08-17 13:00:59 +0200

    add requirement file

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Post
 
-# Register your models here.
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Basic Info', {'fields': ['author']}),
+        ('context', {'fields': ['title', 'content', ]}),
+        ('time', {'fields': ['post_date']}),
+
+    ]
+    list_display = ('title','author','post_date')
+    list_filter = ['post_date']
+    search_fields = ['title']
+
+
+admin.site.register(Post, PostAdmin)
